b40f32b0378aa6d0c9c69f0ab2e7fcc564fdf743
NathanChauveau <nathan.chauveau@eduvaud.ch>
2025-12-19 15:46:46 +0100

    feat: add all routes

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
-
+use App\Bucket\CreateObject;
+use App\Bucket\DeleteObject;
+use App\Bucket\ExistObject;
 use Mezzio\Application;
 use Mezzio\MiddlewareFactory;
 use Psr\Container\ContainerInterface;
 use App\Bucket\ListObject;
+use App\Bucket\ShareObject;
+use App\Bucket\UpdateObject;
 use Mezzio\Helper\BodyParams\BodyParamsMiddleware;
 
 /**
@@ -42,5 +46,10 @@ use Mezzio\Helper\BodyParams\BodyParamsMiddleware;
 
 
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
-    $app->get('/api/v1/objects', ListObject::class, 'api.v1.objects'); #todo check comment les routes sont réalisé, a fouiller plus -> https://docs.mezzio.dev/mezzio/v3/features/container/intro/
+    $app->get('/api/v1/objects', ListObject::class, 'api.v1.objects.list');
+    $app->post('/api/v1/objects', CreateObject::class, 'api.v1.objects.create');
+    $app->get('/api/v1/objects/share',  ShareObject::class, 'api.v1.objects.share');    
+    $app->get('/api/v1/objects/exist', ExistObject::class, 'api.v1.objects.exist');
+    $app->route('/api/v1/objects', DeleteObject::class, ['DELETE'],'api.v1.objects.delete' ); //might need something
+    $app->route('/api/v1/objects', UpdateObject::class,  ['PATCH', 'PUT'],'api.v1.objects.update' ); //might need something
 };
---
+++ src/App/Bucket/ExistObject.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bucket;
+
+#todo -> current step : $service injection in controller without calling listObjects | next step : be able to call service from controller
+
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use \Psr\Http\Message\ResponseInterface;
+
+class ExistObject implements RequestHandlerInterface
+# this comment is a reminder of the road of a request -> router to controller to service to interface ("X" Implem. decide by factory) to "X"AdapterImpl to SDK/mock
+# do rest of method after 1st livrable
+# might need to think how to do the rest of the methods later
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        //
+        /*$remoteSrc = $request->getQueryParams()['remoteSrc'];
+        return $this->listObjects($remoteSrc);*/
+        return new JsonResponse(['status' => 'ok']);
+    }
+
+    public function listObjects(string $remoteSrc)
+    {
+       /*$result = $this->service->list($remoteSrc);
+
+        return $result;*/
+    }
+}
