7c737eeda14eb8b684042666a35ab03670d24af2
Nathan <nathan.chauveau@eduvaud.ch>
2026-01-09 09:57:12 +0100

    chore: add AWS credential

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,4 +4,13 @@ return [
     'app' => [
         'name' => 'MyMicroService',
     ],
+    'aws' => [
+    'credentials' => [
+        'key'    => getenv('AWS_ACCESS_KEY_ID'),
+        'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+    ],
+    'region' => 'eu-central-1',
+    'version' => 'latest',
+    'bucket' => 'bi1-nathan',
+    ],
 ];
