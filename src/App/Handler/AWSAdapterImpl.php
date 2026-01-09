a64fa3bf988abe62117dc4856a05b9004d31a5f1
Nathan <nathan.chauveau@eduvaud.ch>
2026-01-09 10:01:25 +0100

    feat: add function upload for test only (also alr add in their handler)

--- src/App/Handler/AWSAdapterImpl.php
+++ src/App/Handler/AWSAdapterImpl.php
@@ -6,18 +6,22 @@ namespace App\Handler;
 
 use App\Handler\BucketAdapter;
 use AppTest\Adapter\MockGCPSDK;
+use App\Handler\IAWSClient;
 
 class AWSAdapterImpl implements BucketAdapter 
 {
-    private $awsClient;
+    public function __construct(
+        private IAWSClient $client,
+        private string $bucket
+    ) {}
 
-    public function __construct($sdk)
+    public function upload(string $remote, string $content): void
     {
-        $this->sdk = $sdk;
+        $this->client->putObject($this->bucket, $remote, $content);
     }
     public function list(string $remoteSrc): array  
     {
-        return $this->sdk->listObjects($remoteSrc);
+        return $this->client->listObjects($this->bucket, $remoteSrc);
     }
 
 }
