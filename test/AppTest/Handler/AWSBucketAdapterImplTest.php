8641e743c1bb17382aa7e8aeb116ba89c02998f8
Nathan <nathan.chauveau@eduvaud.ch>
2026-01-09 10:38:37 +0100

    test: add code to the rest of tests (also add some missing ones)

--- test/AppTest/Handler/AWSBucketAdapterImplTest.php
+++ test/AppTest/Handler/AWSBucketAdapterImplTest.php
@@ -13,27 +13,47 @@ use App\Handler\IAWSClient;
 
 final class AWSBucketAdapterImplTest extends TestCase
 {
-    public function testListReturnListObjectSucces(): void
+    private $mockClient;
+    private $adapter;
+    private string $bucket = 'test-bucket';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        //given -> prepare the AWS mock
+        $this->mockClient = $this->createMock(IAWSClient::class);
+        $this->adapter = new AWSAdapterImpl($this->mockClient, $this->bucket);
+    }
+    public function testUploadObjectSuccess(): void
     {
         //given prepare mock sdk
-        $mockClient = $this->createMock(IAWSClient::class);
-        //when execute list function
-        $mockClient->expects($this->once())
+        $fileName = 'file.txt';
+        $content = 'Hello World';
+        //when execute the function
+        $this->mockClient->expects($this->once())
             ->method('putObject')
-            ->with(
-                'test-bucket',
-                'file.txt',
-                'content'
-            );
-        $adapter = new AWSAdapterImpl(
-            $mockClient,
-            'test-bucket'
-        );
-        $adapter->upload('file.txt', 'content');
-        //then getting list of objects in the selected folder
+            ->with($this->bucket, $fileName, $content);
+        //then the function is executed without errors
+        $this->adapter->upload($fileName, $content);
     }
 
-  /*  public function testDownloadReturnsContent(): void
+    public function testUploadObjectFail(): void
+    {
+        //given
+        $fileName = 'file.txt';
+        $content = 'Hello World';
+        //when
+        $this->mockClient->method('putObject')
+            ->willThrowException(new \RuntimeException('Upload failed'));
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Upload failed');
+        //then
+        $this->adapter->upload($fileName, $content);
+    }
+
+    public function testDownloadReturnsContent(): void
     {
         $mockClient = $this->createMock(IAWSClient::class);
 
@@ -48,94 +68,159 @@ final class AWSBucketAdapterImplTest extends TestCase
 
         $this->assertSame('hello', $adapter->download('file.txt'));
     }
-*/
+
+    public function testListReturnListObjectSuccess(): void
+    {
+        //given
+        $expectedList = ['file1.txt', 'file2.txt'];
+        $remoteSrc = 'folder/';
+
+        //when
+        $this->mockClient->expects($this->once())
+            ->method('listObjects')
+            ->with($this->bucket, $remoteSrc)
+            ->willReturn($expectedList);
+
+        $result = $this->adapter->list($remoteSrc);
+        //then
+        $this->assertEquals($expectedList, $result);
+    }
+
     public function testListReturnListObjectFail(): void
     {
-        //given
-        $mockSdk = new MockGCPSDK();
-
-        $adapter = new GCPAdapterImpl($mockSdk);
-        $service = new BucketService($adapter);
+        //given 
+        $remoteSrc = 'folder/';
         //when
-        $result = $service->list('/UnknownFolder');
+        $this->mockClient->method('listObjects')
+            ->willThrowException(new \RuntimeException('List failed'));
+
+        $this->expectException(\RuntimeException::class);
         //then
-        $this->assertEquals(['documents', 'images'], $result);
+        $this->adapter->list($remoteSrc);
+
         //might need to be changed into a expected exception when real sdk is implemented
         //need to check what if no folder is found in real sdk
-    }
-
-    public function testCreateObjectSuccess(): void
-    {
-        //given
-
-        //when
-
-        //then
-    }
-    public function testCreateObjectFail(): void
-    {
-        //given
-
-        //when
-
-        //then
     }
 
     public function testUpdateObjectSuccess(): void
     {
         //given
-
+        $fileName = 'file.txt';
+        $content = 'Updated content';
         //when
-
+        $this->mockClient->expects($this->once())
+            ->method('updateObject')
+            ->with($this->bucket, $fileName, $content);
         //then
+        $this->adapter->update($fileName, $content);
     }
     public function testUpdateObjectFail(): void
     {
         //given
-
+        $fileName = 'file.txt';
+        $content = 'Updated content';
         //when
+        $this->mockClient->method('updateObject')
+            ->willThrowException(new \RuntimeException('Update failed'));
 
+        $this->expectException(\RuntimeException::class);
         //then
+        $this->adapter->update($fileName, $content);
     }
     public function testDownloadObjectSuccess(): void
     {
         //given
-
+        $fileName = 'file.txt';
+        $expectedContent = 'File content';
         //when
-
+        $this->mockClient->method('getObject')
+            ->with($this->bucket, $fileName)
+            ->willReturn($expectedContent);
         //then
+        $this->assertEquals($expectedContent, $this->adapter->download($fileName));
     }
     public function testDownloadObjectFail(): void
     {
         //given
+        $fileName = 'file.txt';
 
         //when
+        $this->mockClient->method('getObject')
+            ->willThrowException(new \RuntimeException('Download failed'));
 
+        $this->expectException(\RuntimeException::class);
         //then
+        $this->adapter->download($fileName);
     }
     public function testShareObjectSuccess(): void
     {
         //given
+        $fileName = 'file.txt';
+        $duration = 3600;
+        $expectedUrl = 'https://temp-url';
 
         //when
-
+        $this->mockClient->expects($this->once())
+            ->method('shareObject')
+            ->with($this->bucket, $fileName)
+            ->willReturn($expectedUrl);
         //then
+        $result = $this->adapter->share($fileName, $duration);
+
+        $this->assertEquals($expectedUrl, $result);
     }
     public function testShareObjectFail(): void
     {
         //given
+        $fileName = 'file.txt';
+        $duration = 3600;
 
         //when
+        $this->mockClient->method('shareObject')
+            ->willThrowException(new \RuntimeException('Share failed'));
 
+        $this->expectException(\RuntimeException::class);
         //then
+        $this->adapter->share($fileName, $duration);
     }
 
     public function testDoesObjectExistSuccess(): void
     {
         //given
+        $fileName = 'file.txt';
+        //when
+        $this->mockClient->method('doesObjectExist')
+            ->with($this->bucket, $fileName)
+            ->willReturn(true);
+        $reflection = new \ReflectionClass($this->adapter);
+        $method = $reflection->getMethod('doesExist');
+        $method->setAccessible(true);
+        //then
+        $this->assertTrue($method->invoke($this->adapter, $fileName));
+    }
+    public function testDeleteObjectSuccess(): void
+    {
+        //given
+        $fileName = 'file.txt';
+        //when
+        $this->mockClient->expects($this->once())
+            ->method('deleteObject')
+            ->with($this->bucket, $fileName);
+        //then
+        $this->adapter->delete($fileName);
+    }
+    public function testDeleteObjectFail(): void
+    {
+        //given
+        $fileName = 'file.txt';
+
 
         //when
+        $this->mockClient->method('deleteObject')
+            ->willThrowException(new \RuntimeException('Delete failed'));
 
+        $this->expectException(\RuntimeException::class);
         //then
+        $this->adapter->delete($fileName);
     }
 }
